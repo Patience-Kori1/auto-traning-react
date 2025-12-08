23eb879905c59c7ee5d04ccc041c432b5f82f804
Patience-Kori1 <koribiram@yahoo.fr>
2025-12-08 21:42:26 +0100

    feat(Lesson - Your First Component): Mastery of basic React component creation - Exporting components using default and named exports - Defining component functions following PascalCase convention - Implementing JSX structure with an explicit return statement - Composing components through nesting and reuse

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import FirstComponent from "./components/2_Describing_UI/1_FirstComponent"
 import Gallery from "./components/I_Installation/lesson1"
 import Lesson2 from "./components/I_Installation/Lesson2"
 import Lesson2B from "./components/I_Installation/Lesson2B"
@@ -7,8 +8,13 @@ import Lesson2E_A from "./components/I_Installation/Lesson2E_A"
 import './index.css'
 export default function App() {
   return(
-    <>
-      <h2> Lesson 2D : Typage du hook useContext</h2>
+  <>
+    <>{/* Section II. Décrire L'UI*/}
+      <FirstComponent/>
+    </>
+    <>{/* Section I. Installation*/}
+      
+      {/* <h2> Lesson 2D : Typage du hook useContext</h2>
       <Lesson2E_A/>
       <h2> Lesson 2D : Typage du hook useReduce</h2>
       <Lesson2D/>
@@ -19,7 +25,8 @@ export default function App() {
       <h2> Lesson 2A : TypeScript pour les composant React</h2>
       <Lesson2/>
       <h2> Lesson 1 : First component</h2>
-      <Gallery/>
+      <Gallery/> */}
     </>
+  </>
   )
 }
---
+++ src/components/2_Describing_UI/1_FirstComponent.tsx
@@ -0,0 +1,38 @@
+
+function FirstComponent() {
+  return (
+    <article>
+      <h3>Mon composant</h3>
+      <ol>
+          <li>Les composants : les blocs de construction de l’UI</li>
+          <li>Définir un composant</li>
+          <li>Utiliser un composant</li>
+      </ol>
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    </article>
+  )
+}
+
+export function Profile() {
+  return (
+    <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+  )
+}
+
+export default function Galery() {
+  return (
+    <>
+      <h3>Scientifiques de renom</h3>
+      <Profile/>
+      <Profile/>
+      <Profile/>
+      <FirstComponent/>
+    </>
+  )
+}
