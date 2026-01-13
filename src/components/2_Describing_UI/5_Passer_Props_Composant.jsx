21ea7fbb2d1f818b841a7daf808db1c4d1ad5f70
Patience-Kori1 <koribiram@yahoo.fr>
2026-01-13 22:53:20 +0100

    docs(react-props): passing props from parent to child components - Passed custom props from a parent component to a child component - Used multiple prop types (objects and numbers) - Read and consumed props in the child component using destructuring - Understood props as function arguments in React components

--- src/components/2_Describing_UI/5_Passer_Props_Composant.jsx
+++ src/components/2_Describing_UI/5_Passer_Props_Composant.jsx
@@ -1,17 +1,60 @@
-function Avatar() {
+// function Avatar() {
+//   return (
+//     <img
+//       className="avatar"
+//       src="https://i.imgur.com/1bX5QH6.jpg"
+//       alt="Lin Lanying"
+//       width={100}
+//       height={100}
+//     />
+//   );
+// }
+
+// export default function Passer_Props_A_Composant() {
+//   return (
+//     <Avatar 
+//     />
+//   );
+// }
+
+import { getImageUrl } from './support_files/Utils'
+
+function Avatar({ person, size }) {
   return (
     <img
       className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
     />
   );
 }
 
 export default function Passer_Props_A_Composant() {
   return (
-    <Avatar />
+    <div>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma',
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
   );
 }
