5a0345838445fa78ca0655023bee6cc0338a12d9
Patience-Kori1 <koribiram@yahoo.fr>
2026-02-05 18:21:50 +0100

    Finalisation du défi : Extraire un composant react

--- src/components/2_Describing_UI/support_files/for_5/Profile.jsx
+++ src/components/2_Describing_UI/support_files/for_5/Profile.jsx
@@ -1,7 +1,22 @@
-export default function Profile() {
+import { getImageUrl } from './Utils';
+export default function Profile({personItem}) {
+
     return(
-        <div>
-            <h3>Maria</h3>
+        <div className="profile">
+            <h2>{personItem.name}</h2>
+            <img 
+                src={getImageUrl(personItem)}
+                alt={personItem.name}
+                className="avatar"
+                width={70}
+                height={70}
+            />
+            <ul>
+                <li><b>Profession : </b> {personItem.job}</li>
+                <li><b>Récompense : {personItem.rewardNumber} </b> {personItem.reward}</li>
+                <li><b>A découvert :</b> {personItem.discovery}</li>
+            </ul>
+
         </div>
     )
 }
