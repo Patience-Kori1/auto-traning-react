c2db2f96674b255716dc57ff59acae4a9a96f753
Patience-Kori1 <koribiram@yahoo.fr>
2026-02-09 18:26:02 +0100

    refactor(react-props): Completed challenge 3 - Optimized Card component using children - Moved card-content structure inside the Card component - Reduced JSX duplication in the parent component - Improved readability and reusability of the Card wrapper

--- src/components/2_Describing_UI/support_files/for_5/defi_3_Passer_JSX_Prop_Children.jsx
+++ src/components/2_Describing_UI/support_files/for_5/defi_3_Passer_JSX_Prop_Children.jsx
@@ -1,7 +1,9 @@
 export function Card({children}) {
     return (
         <div className="card">
+          <div className="card-content">
             {children}
+          </div>
         </div>
     )
 }
@@ -10,22 +12,18 @@ export default function Profile3() {
   return (
     <div>
       <Card>
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="https://i.imgur.com/OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
      </Card>
       <Card>
-        <div className="card-content">
           <h1>À propos</h1>
           <p>Aklilu Lemma était un scientifique éthiopien renommé qui a découvert le traitement naturel de la bilharziose.</p>
-        </div>
       </Card>
     </div>
   );
