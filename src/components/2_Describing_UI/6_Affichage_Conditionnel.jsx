784f5016cd59d3097f004acdbecc646b63f698d1
Patience-Kori1 <koribiram@yahoo.fr>
2026-02-11 15:51:45 +0100

    feat(affichage-conditionnel): implémentation du rendu conditionnel avec if/else - Création du composant Item avec props name et isPacked - Implémentation d’une condition if/else pour afficher un indicateur visuel (✅) - Intégration du composant dans Affichage_Conditionnel avec différents cas

--- src/components/2_Describing_UI/6_Affichage_Conditionnel.jsx
+++ src/components/2_Describing_UI/6_Affichage_Conditionnel.jsx
@@ -1,7 +1,32 @@
+import "../../App.css"
+function Item({ name, isPacked }) {
+    if (isPacked) {
+        return <li className="item">{name} ✅</li>;
+    }
+    return <li className="item">{name}</li>;
+}
+
 export default function Affichage_Conditionnel() {
     return(
         <div>
-            <h2>Affichage conditionnel</h2>
+            <section>
+                <h3>Liste d’affaires de Sally Ride</h3>
+                <ul>
+                    <Item
+                        isPacked={true}
+                        name="Combinaison spatiale"
+                    />
+                    <Item
+                        isPacked={true}
+                        name="Casque à feuille d’or"
+                    />
+                    <Item
+                        isPacked={false}
+                        name="Photo de Tam"
+                    />
+                </ul>
+            </section>    
         </div>
     )
 }
+
