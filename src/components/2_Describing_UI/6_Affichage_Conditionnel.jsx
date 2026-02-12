d5fedb9f7987ffc19d9d58c50ef8de6c844171cb
Patience-Kori1 <koribiram@yahoo.fr>
2026-02-12 18:17:07 +0100

    feat(conditional-rendering): implement conditional rendering with null exclusion - Updated Item component logic - Used if statement to return null for conditional exclusion - Demonstrated conditional hiding of list items

--- src/components/2_Describing_UI/6_Affichage_Conditionnel.jsx
+++ src/components/2_Describing_UI/6_Affichage_Conditionnel.jsx
@@ -1,9 +1,18 @@
 import "../../App.css"
 function Item({ name, isPacked }) {
+
+    // 1. Rendu conditionnel avec deux possibilités d'affichage de deux branches (les isPacked avec un done et les false sans done)
+    // if (isPacked) {
+    //     return <li className="item">{name} ✅</li>;
+    // }
+    // return <li className="item">{name}</li>;
+
+    // 2. Rendu conditionnel avec une possibilité d'exclure en le rendant nulle ou inclure des portions de JSX
     if (isPacked) {
-        return <li className="item">{name} ✅</li>;
+        return null;
     }
     return <li className="item">{name}</li>;
+
 }
 
 export default function Affichage_Conditionnel() {
@@ -13,7 +22,7 @@ export default function Affichage_Conditionnel() {
                 <h3>Liste d’affaires de Sally Ride</h3>
                 <ul>
                     <Item
-                        isPacked={true}
+                        isPacked={false}
                         name="Combinaison spatiale"
                     />
                     <Item
