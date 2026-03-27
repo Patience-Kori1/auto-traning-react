18e1645310ea66c7be47b66f6d1cdcacf85ff93a
Patience-Kori1 <koribiram@yahoo.fr>
2026-03-27 23:34:04 +0100

    style(ui-consistency): Visual harmonization of the second part of chapter 1 using sub-section containers - Added sub-section wrappers to the second part of the First React Component chapter - Applied sub-section styling to standardize subsection layout - Continued improving the global visual consistency of the project

--- src/components/2_Describing_UI/1_FirstComponent.tsx
+++ src/components/2_Describing_UI/1_FirstComponent.tsx
@@ -3,11 +3,13 @@ function FirstComponent() {
   return (
     <article>
       <h3 className="section-title">Mon composant</h3>
-      <ol>
-        <li>Les composants : les blocs de construction de l’UI</li>
-        <li>Définir un composant</li>
-        <li>Utiliser un composant</li>
-      </ol>
+      <div className="sub-section">
+        <ol>
+          <li>Les composants : les blocs de construction de l’UI</li>
+          <li>Définir un composant</li>
+          <li>Utiliser un composant</li>
+        </ol>
+      </div>
       <img
         src="https://i.imgur.com/MK3eW3Am.jpg"
         alt="Katherine Johnson"
